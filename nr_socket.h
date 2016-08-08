ca0140ae265c8ef2416db6ff1d5906a98f28142a
Ruto <nullruto@protonmail.ch>
2016-08-08 16:21:37 -0400

    Fixed includes

--- nr_socket.h
+++ nr_socket.h
@@ -2,6 +2,23 @@
 // Created by Ruto on 8/4/2016.
 //
 
+#include <stdio.h>
+#ifdef _WIN32
+/* See http://stackoverflow.com/questions/12765743/getaddrinfo-on-win32 */
+#ifndef _WIN32_WINNT
+#define _WIN32_WINNT 0x0501  /* Windows XP. */
+#endif
+#include <winsock2.h>
+#include <Ws2tcpip.h>
+#pragma comment (lib, "Ws2_32.lib")
+#else
+/* Assume that any non-Windows platform uses POSIX-style sockets instead. */
+  #include <sys/socket.h>
+  #include <arpa/inet.h>
+  #include <netdb.h>  /* Needed for getaddrinfo() and freeaddrinfo() */
+  #include <unistd.h> /* Needed for close() */
+#endif
+
 typedef void (*nr_socket_callback)(int, char*);
 
 int nr_socket_create(void);
